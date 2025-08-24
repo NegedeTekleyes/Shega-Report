9edd550636873cb9d1093aff866e70758f43a597
Negede <negedetekleyes@gmail.com>
2025-08-25 00:13:33 +0300

    Loading Screen

---
+++ components/loadingScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
+import { useLanguage } from '@/providers/language-providers';
+
+// import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+export default function LoadingScreen() {
+  const { t } = useLanguage();
+ return (
+    <View className="flex-1 justify-center items-center bg-green-600">
+      <ActivityIndicator size="large" color="white" />
+      <Text className="text-white mt-4 text-lg">{t('loading')}...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
