6242a626817c943714756963299e9cc2570d56de
Negede <negedetekleyes@gmail.com>
2025-11-09 20:58:13 +0300

    Update route path of technicians page

--- app/(technician)/_layout.tsx
+++ app/(technician)/_layout.tsx
@@ -1,18 +1,18 @@
-// import { useAuth } from "@/providers/auth-providers";
-// import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@/providers/auth-providers";
+import { Redirect, Stack } from "expo-router";
 
+export default function TechnicianLayout() {
+  const { user } = useAuth();
 
-// export default function TechnicianLayout() {
-//     const {user} = useAuth()
-
-//     // protect technicial routes onlt technician can access
-//     if(user?.role !== 'technician'){
-//         return <Redirect href="/(auth)/welcome"/>
-//     }
-//     return(
-//         <Stack>
-//             <Stack.Screen name="index" options={{title: 'Technician Home'}}/>
-
-//         </Stack>
-//     )
-// }
+  // Case-insensitive check
+  if (user?.role?.toUpperCase() !== "TECHNICIAN") {
+    return <Redirect href="/(auth)/welcome" />;
+  }
+  
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Technician Home" }} />
+      <Stack.Screen name="TasksScreen" options={{ title: "My Tasks" }} />
+    </Stack>
+  );
+}
